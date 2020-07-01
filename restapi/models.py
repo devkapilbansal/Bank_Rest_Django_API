b9ca67b8f638dc532cf743d1631a2bdc30da95ed
vishal sharma <vishalsh533@gmail.com>
2020-07-02 02:02:58 +0530

    Update models.py

--- restapi/models.py
+++ restapi/models.py
@@ -17,12 +17,12 @@ class Branch(models.Model):
         db_table = 'branches'
 
     ifsc = models.CharField(primary_key=True, max_length=11, db_column='ifsc')
-    bank_id = models.IntegerField(primary_key=True, db_column='bank_id')
-    branch = models.CharField(primary_key=True, max_length=74, db_column='branch')
-    address = models.CharField(primary_key=True, max_length=195, db_column='address')
-    city = models.CharField(primary_key=True, max_length=50, db_column='city')
-    district = models.CharField(primary_key=True, max_length=50, db_column='district')
-    state = models.CharField(primary_key=True, max_length=26, db_column='state')
+    bank_id = models.IntegerField( db_column='bank_id')
+    branch = models.CharField( max_length=74, db_column='branch')
+    address = models.CharField( max_length=195, db_column='address')
+    city = models.CharField( max_length=50, db_column='city')
+    district = models.CharField( max_length=50, db_column='district')
+    state = models.CharField( max_length=26, db_column='state')
 
     def __str__(self):
         return "{} {} {} {} {} {} {}".format(self.ifsc, self.bank_id, self.branch, self.address, self.city, self.district, self.state)
@@ -33,12 +33,12 @@ class BankBranch(models.Model):
         db_table = 'bank_branches'
 
     ifsc = models.CharField(primary_key=True, max_length=11, db_column='ifsc')
-    bank_id = models.IntegerField(primary_key=True, db_column='bank_id')
-    branch = models.CharField(primary_key=True, max_length=74, db_column='branch')
-    address = models.CharField(primary_key=True, max_length=195, db_column='address')
-    city = models.CharField(primary_key=True, max_length=50, db_column='city')
-    district = models.CharField(primary_key=True, max_length=50, db_column='district')
-    state = models.CharField(primary_key=True, max_length=26, db_column='state')
+    bank_id = models.ForeignKey(Bank, on_delete=models.CASCADE))
+    branch = models.CharField( max_length=74, db_column='branch')
+    address = models.CharField( max_length=195, db_column='address')
+    city = models.CharField( max_length=50, db_column='city')
+    district = models.CharField( max_length=50, db_column='district')
+    state = models.CharField( max_length=26, db_column='state')
     bank_name = models.CharField(max_length=49, db_column='bank_name')
 
     def __str__(self):
