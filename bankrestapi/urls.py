dc77fe66ed830c25a6a0b179266bffc7ebc4912e
vishal sharma <vishalsh533@gmail.com>
2020-07-02 02:04:22 +0530

    Update urls.py

--- bankrestapi/urls.py
+++ bankrestapi/urls.py
@@ -14,7 +14,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path, include
+from rest_framework_jwt.views import obtain_jwt_token
 
 urlpatterns = [
     path('admin/', admin.site.urls),
