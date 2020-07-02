eeb1fc595dff4c8b0c03f3c8ecc5770ab0a84b74
vishal sharma <vishalsh533@gmail.com>
2020-07-02 12:24:39 +0530

    Update settings.py

--- bankrestapi/settings.py
+++ bankrestapi/settings.py
@@ -131,11 +131,6 @@ WSGI_APPLICATION = 'bankrestapi.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ['DB_NAME'],
-        'USER': os.environ['DB_USER'],
-        'PASSWORD': os.environ['DB_PASSWORD'],
-        'HOST': os.environ['DB_HOST'],
-        'PORT': os.environ['DB_PORT'],
     }
 }
 
