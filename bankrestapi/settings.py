b5bd5a1d963807177a284a4e38aa2b8680126a08
vishal sharma <vishalsh533@gmail.com>
2020-07-02 18:11:12 +0530

    Update settings.py

--- bankrestapi/settings.py
+++ bankrestapi/settings.py
@@ -65,8 +65,8 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.TokenAuthentication',
     ],
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
-        'rest_framework.permissions.IsAdminUser'
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly',
+        'rest_framework.permissions.IsAdminUser',
     ],
 }
 
